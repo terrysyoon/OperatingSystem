eee80d4104b7d56425949d21154b79d16202b6fd
Sungbae Yoon <yoonsb@hanyang.ac.kr>
2023-05-14 15:58:18 +0900

    FIX: list

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -585,7 +585,7 @@ pmanagerList(void) {
       state = "???";
     
     uint pages = (PGROUNDUP(p->stackSize));
-    cprintf("%s %d %s %u %u %u\n", p->name, p->pid, state, p->sz, p->memorylimit, pages);
+    cprintf("%s %d %s %d %d %d\n", p->name, p->pid, state, pages, p->sz, p->memorylimit);
   
     if(p->state == SLEEPING){
       getcallerpcs((uint*)p->context->ebp+2, pc);
