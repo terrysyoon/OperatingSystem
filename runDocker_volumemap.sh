cacbfd4ae0d63bfed5137ee701a6dcbbed374397
Sungbae Yoon <yoonsb@hanyang.ac.kr>
2023-05-16 16:41:03 +0900

    d

---
+++ runDocker_volumemap.sh
@@ -0,0 +1,3 @@
+docker run -d -v "/Users/terry/Library/Mobile Documents/com~apple~CloudDocs/5th Semester/Operating Systems/mac_xv6_Workspace/OperatingSystem":/container/mac_workspace os_xv6
+#docker exec -it e621077e655c /bin/bash
+#cd /container/mac_workspace
