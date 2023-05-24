2b99af7243ce6ada3297b0c7cd174cb8cb21e86e
Sungbae Yoon <yoonsb@hanyang.ac.kr>
2023-05-25 00:29:23 +0900

    debug: test called join twice

--- xv6-public/pthreadtest.c
+++ xv6-public/pthreadtest.c
@@ -44,15 +44,18 @@ int main()
 	thread_create(&p1, myThread, "A");
 	printf(1, "main: create p1: %d\n",p1);
 	//printf(1, "1 ");
+	
+	
 	thread_create(&p2, myThread, "B");
 	//printf(1, "done! \n");
 	// Wait for threads
 	printf(1, "p1 %d joined: %d\n",p1,thread_join(p1, NULL));
 	//printf(1, "join 1~ ");
-	thread_join(p2, NULL);
+	//thread_join(p2, NULL);
+	printf(1, "p2 %d joined: %d\n",p2,thread_join(p2, NULL));
 	//printf(1, "2!\n ");	
 	// Done
-	printf(1, "p2 joined: %d\n",p2,thread_join(p2, NULL));
+	//printf(1, "p2 joined: %d\n",p2,thread_join(p2, NULL));
 	printf(1, "main: done with both (counter = %d)\n", counter);
 	//return 0;
 	exit();
