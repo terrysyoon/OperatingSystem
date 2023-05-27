0c226cccb609159acb520a5b7e394df00785ed5d
Sungbae Yoon <yoonsb@hanyang.ac.kr>
2023-05-27 12:32:24 +0900

    d

--- xv6-public/proc.c
+++ xv6-public/proc.c
@@ -953,7 +953,7 @@ exec_remove_thread(char *path, char **argv) {
     if(curproc->tcb.parentProc->state == SLEEPING)
       curproc->tcb.parentProc->state = RUNNABLE;
     curproc->state = SLEEPING;
-    procdump();
+    //procdump();
     cprintf("exec_remove_thread> pid: %d calling sched\n", curproc->pid);
     sched();
     release(&ptable.lock);
